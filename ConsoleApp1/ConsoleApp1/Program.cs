2ba23bf6158ecd03fa40a98bf58f5eabec3ce171
song <song@DESKTOP-C6V3TNA>
2017-10-21 00:00:49 +0700

    first year semester 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("year 255x semester 1,i am an engedu freshman.")
+        }
+    }
+}
